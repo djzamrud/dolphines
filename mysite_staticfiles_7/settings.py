001a1022826a18c7fe60b9377895f92acf034a52
djzamrud <asepsaifulzamrud@gmail.com>
2026-06-13 17:57:25 +0700

    nambahin allow host

--- mysite_staticfiles_7/settings.py
+++ mysite_staticfiles_7/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w4klo4p2tbuy_f@&q3569s8j(4xq6tivs(%6=zm&a&am7^23=t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'blacksider.pythonanywhere.com']
 
 
 # Application definition
